cecdd22ccbd74994bbd9f7dfa07f9a16227c0499
onlyVishesh <visheshmaurya14@gmail.com>
2024-10-24 22:46:15 +0530

    Added ep5

---
+++ Season 1/05 | Diving into the NodeJS github repo/app.js
@@ -0,0 +1,9 @@
+//require("/path");
+// All the code of the module is wrapped inside the function (IIFE)
+//IIFE - Imediately Invoked Function Expression
+
+(function () {
+  // aAl modules are converted like this before executing 
+  // All the code of the module is runs here
+
+})();
