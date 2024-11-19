0a79445506675eb6b52443acc095c5f774f5ad05
onlyVishesh <visheshmaurya14@gmail.com>
2024-11-19 14:56:21 +0530

    Database manuplation api end points created
    Added post, get, delete and patch requests to manuplate data of database

--- Season 2/07 | Diving into the APis/src/app.js
+++ Season 2/07 | Diving into the APis/src/app.js
@@ -18,6 +18,111 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// search user by username or email
+app.get("/user", async (req, res) => {
+  try {
+    const { email, username } = req.body;
+    const userId = email || username;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ error: "Email or username must be provided" });
+    }
+
+    const user = await User.findOne({
+      $or: [{ email: userId }, { username: userId }],
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+// feed api - to get data of the all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    if (users.length === 0) {
+      return res.status(404).json({ error: "User no user exist" });
+    }
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+// delete a user by username or email
+app.delete("/user", async (req, res) => {
+  try {
+    const { email, username } = req.body;
+    const userId = email || username;
+
+    if (!userId) {
+      return res
+        .status(404)
+        .json({ error: "Email or username must be provided" });
+    }
+
+    const user = await User.findOneAndDelete({
+      $or: [{ username: userId }, { email: userId }],
+    });
+
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({
+        deletedUser: `User with username : ${user.username} and email : ${user.email} has been deleted`,
+      });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+// update a user by username or email
+app.patch("/user", async (req, res) => {
+  try {
+    const { email, username } = req.body;
+    const userId = email || username;
+
+    const data = req.body;
+    if (!userId) {
+      return res
+        .status(404)
+        .json({ error: "Email or username must be provided" });
+    }
+
+    const user = await User.findOneAndUpdate(
+      {
+        $or: [{ username: userId }, { email: userId }],
+      },
+      data
+    );
+
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+    res.status(200).json({
+      updatedUser: `User with username : ${user.username} and email : ${user.email} has been Updated`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
 // first connecting to database the allowing requests
 connectDB()
   .then(() => {
