67ad964b223baafaa1fc46c00d36ab63c2a6edb4
onlyVishesh <visheshmaurya14@gmail.com>
2024-11-24 22:43:09 +0530

    Created JWT token for login

---
+++ Season 2/10 | Authentication, JWT & Cookies/src/app.js
@@ -0,0 +1,208 @@
+const express = require("express");
+const { connectDB } = require("./config/database");
+const app = express();
+const User = require("./models/user");
+const {
+  validateSignUpData,
+  validateLoginData,
+} = require("./utils/validation");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.post("/signup", async (req, res) => {
+  try {
+    validateSignUpData(req);
+    const { username, firstName, lastName, email, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      username,
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
+    await user.save();
+    res.status(200).send("User Added successfully");
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    validateLoginData(req);
+    const { username, email, password } = req.body;
+    const userId = email || username;
+
+    const user = await User.findOne({
+      $or: [{ email: userId }, { username: userId }],
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid Credential" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, this.password);
+
+    if (isPasswordValid) {
+      
+      // JWT token created at user model
+      const token = jwt.sign({ _id: this._id }, process.env.secretJWT, {
+        expiresIn: "3d",
+      });
+
+      // add the token to cookie and send back to user
+      res.cookie("token", token);
+      res.status(200).send("login successful");
+    } else {
+      throw new Error("Invalid Credential");
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  try {
+    const user = req.user;
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const { email, username } = req.body;
+    const userId = email || username;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ error: "Email or username must be provided" });
+    }
+
+    const user = await User.findOne({
+      $or: [{ email: userId }, { username: userId }],
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong " + err.message });
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    if (users.length === 0) {
+      return res.status(404).json({ error: "User no user exist" });
+    }
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong " + err.message });
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const { email, username } = req.body;
+    const userId = email || username;
+
+    if (!userId) {
+      return res
+        .status(404)
+        .json({ error: "Email or username must be provided" });
+    }
+
+    const user = await User.findOneAndDelete({
+      $or: [{ username: userId }, { email: userId }],
+    });
+
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    res.status(200).json({
+      deletedUser: `User with username : ${user.username} and email : ${user.email} has been deleted`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong " + err.message });
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const { email, username } = req.body;
+    const userId = email || username;
+
+    const ALLOWED_UPDATE = [
+      "username",
+      "firstName",
+      "lastName",
+      "password",
+      "avatar",
+      "about",
+      "skills",
+      "dateOfBirth",
+      "gender",
+      "role",
+      "status",
+    ];
+    const data = req.body;
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATE.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allow");
+    }
+
+    if (!userId) {
+      return res
+        .status(404)
+        .json({ error: "Email or username must be provided" });
+    }
+
+    const user = await User.findOneAndUpdate(
+      {
+        $or: [{ username: userId }, { email: userId }],
+      },
+      data,
+      { runValidators: true }
+    );
+
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+    res.status(200).json({
+      updatedUser: `User with username : ${user.username} and email : ${user.email} has been Updated`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong " + err.message });
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("database connection establish");
+    app.listen(3000, () => {
+      console.log("Server is listening on port 3000...");
+    });
+  })
+  .catch((err) => console.error(err));
---
+++ Season 2/10 | Authentication, JWT & Cookies/src/models/user.js
@@ -0,0 +1,123 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require("dotenv").config;
+
+// Define the User schema
+// Added new validations and SchemaTypes
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minLength: [3, "Username too small"],
+      maxLength: [30, "Username too large"],
+      validate: {
+        validator: function (value) {
+          return /^[a-zA-Z0-9_]+$/.test(value);
+        },
+        message:
+          "Username can only contain alphabets, numbers, and underscores.",
+      },
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: [25, "First name too large"],
+      match: /^[a-zA-Z]+$/,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      maxLength: [25, "Last name too large"],
+      match: /^[a-zA-Z]+$/,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return validator.isEmail(value);
+        },
+        message: "Invalid email address",
+      },
+      lowercase: true,
+      trim: true,
+      minLength: [3, "Email too small"],
+      maxLength: [30, "Email too large"],
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return validator.isStrongPassword(value);
+        },
+        message: "Password is not strong",
+      },
+    },
+    avatar: {
+      type: String,
+      trim: true,
+      validate: {
+        validator: function (value) {
+          return validator.isURL(value);
+        },
+        message: "Give string is not an URL",
+      },
+    },
+    about: {
+      type: String,
+      maxLength: [500, "Too many words"],
+      trim: true,
+    },
+    skills: {
+      type: [String],
+      validate: {
+        validator: function () {
+          return this.skills.length < 16;
+        },
+        message: "Too many Skills, make total skills less that or equal to 15",
+      },
+    },
+    dateOfBirth: {
+      type: Date,
+      // need to add minimum zero year old and max 150 years
+    },
+    age: {
+      type: Number,
+      get: function () {
+        if (!this.dateOfBirth) return null;
+        const diff = Date.now() - this.dateOfBirth.getTime();
+        return Math.floor(diff / (365.25 * 24 * 60 * 60 * 1000));
+      },
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "moderator", "user"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "banned"],
+      default: "active",
+    },
+  },
+  { timestamps: true }
+);
+
+// Export the model
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
