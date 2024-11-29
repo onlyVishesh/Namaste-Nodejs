c888a02b833c549bce3746deed1f874c30f0e867
onlyVishesh <visheshmaurya14@gmail.com>
2024-11-30 01:33:43 +0530

    Added more end points to profileRouter
    Added view profile, edit profile and delete profile apis

--- Season 2/11 | Diving into the APls and express Router/src/routes/profile.js
+++ Season 2/11 | Diving into the APls and express Router/src/routes/profile.js
@@ -19,6 +19,66 @@ profileRouter.get("/feed", async (req, res) => {
   }
 });
 
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateProfileData(req)) {
+      throw new Error("Update not allow");
+    }
+
+    const loggedInUser = req.user;
+    console.log("hi")
+    Object.keys(req.body).forEach(
+      (field) => (loggedInUser[field] = req.body[field])
+    );
+
+    await loggedInUser.save();
+
+    res.status(200).json({
+      message: `${loggedInUser.firstName}, Your profile has been updated`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong " + err.message });
+  }
+});
+
+profileRouter.delete("/user", async (req, res) => {
+  try {
+    const { email, username } = req.body;
+    const userId = email || username;
+
+    if (!userId) {
+      return res
+        .status(404)
+        .json({ error: "Email or username must be provided" });
+    }
+
+    const user = await User.findOneAndDelete({
+      $or: [{ username: userId }, { email: userId }],
+    });
+
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    res.status(200).json({
+      deletedUser: `User with username : ${user.username} and email : ${user.email} has been deleted`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong " + err.message });
+  }
+});
+
 profileRouter.get("/user", async (req, res) => {
   try {
     const { email, username } = req.body;
