e29ab9093777dbc99a528a74ffc28774cddbb915
onlyVishesh <visheshmaurya14@gmail.com>
2024-11-30 01:45:48 +0530

    Added requestRouter

--- Season 2/11 | Diving into the APls and express Router/src/app.js
+++ Season 2/11 | Diving into the APls and express Router/src/app.js
@@ -8,8 +8,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
 app.use("/", authRouter)
+app.use("/", profileRouter)
+app.use("/", requestRouter)
 
 connectDB()
   .then(() => {
