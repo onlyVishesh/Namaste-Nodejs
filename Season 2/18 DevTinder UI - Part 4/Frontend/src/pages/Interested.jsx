4a3d6da1532d5ababc7af94ae9df8b6d76d8688b
onlyVishesh <visheshmaurya14@gmail.com>
2025-01-11 03:31:02 +0530

    Feature: Added Interested and connection section

--- Season 2/18 DevTinder UI - Part 4/Frontend/src/pages/Interested.jsx
+++ Season 2/18 DevTinder UI - Part 4/Frontend/src/pages/Interested.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "sonner";
+import NetworkCard from "../components/NetworkCard";
 
 const Interested = () => {
-  return (
-    <div>Interested</div>
-  )
-}
+  const [interestedRequests, setInterestedRequests] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default Interested
+  const getInterestedRequest = async (currentPage) => {
+    setIsLoading(true);
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_BackendURL}/request/received?page=${currentPage}&limit=10`,
+        { withCredentials: true },
+      );
+      if (res.data.success === false) {
+        toast.error(res.data.message || "An error occurred");
+      } else {
+        setInterestedRequests((prev) => [...prev, ...res.data.user]);
+        setTotalPages(res.data.pagination.totalPages);
+      }
+    } catch (err) {
+      toast.error(err.response?.data?.error || "Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getInterestedRequest(page);
+  }, [page]);
+
+  return (
+    <div className="rounded-md bg-bgSecondary">
+      <h2 className="px-4 py-2 text-2xl font-bold">
+        Interested ({interestedRequests.length})
+      </h2>
+      <hr className="border-textMuted" />
+      <div className="flex flex-col divide-y divide-textMuted">
+        {interestedRequests.length === 0 ? (
+          <div className="py-5 text-center">
+            No Pending Requests exist. Try to{" "}
+            <Link to="/feed" className="font-bold text-primary underline">
+              Explore
+            </Link>{" "}
+            profiles
+          </div>
+        ) : (
+          interestedRequests.map((request) => (
+            <NetworkCard
+              type="invitation"
+              request={request}
+              key={request._id}
+            />
+          ))
+        )}
+      </div>
+      {page < totalPages && (
+        <div className="py-4 text-center">
+          <button
+            onClick={() => setPage((prev) => prev + 1)}
+            className="rounded-md bg-primary px-4 py-2 text-white hover:bg-hover"
+            disabled={isLoading}
+          >
+            {isLoading ? "Loading..." : "Show More"}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Interested;
